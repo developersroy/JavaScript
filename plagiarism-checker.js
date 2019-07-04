5a38e73b12d720e89c8dbbc8593a0b0d581a9d31
developersroy <50220887+developersroy@users.noreply.github.com>
2019-07-05 04:33:47 +0700

    Update plagiarism-checker.js

--- plagiarism-checker.js
+++ plagiarism-checker.js
@@ -3,12 +3,12 @@
 var widgetHtml = '<iframe src="https://www.prepostseo.com/widget/plagiarismchecker" style="border:0;width:100%;min-height:600px;height:100%;"></iframe>';
 var widgetTag = document.getElementById("ppsLink");
 if (widgetTag === null){ 
-	widgetHtml += '<div style="text-align: center; font-size:12px; color:#333;"><p>Plagiarism checker provided by <a href="https://www.prepostseo.com/plagiarism-checker" id="ppsLink" target="_blank"  style="color:#000;">prepostseo.com</a></p></div>';
+	widgetHtml += '<div style="text-align: center; font-size:12px; color:#333;"><p>Plagiarism checker provided by <a href="https://www.ehmasroy.com/" id="ppsLink" target="_blank"  style="color:#000;">ehmasroy.com</a></p></div>';
 	
 } else {
 	var linkUrl = widgetTag.getAttribute("href");	
 	if(linkUrl !== 'https://www.prepostseo.com/plagiarism-checker'){
-		widgetHtml += '<div style="text-align: center; font-size:12px; color:#333;"><p>Plagiarism checker provided by <a href="https://www.prepostseo.com/plagiarism-checker" id="ppsLink" target="_blank"  style="color:#000;">prepostseo.com</a></p></div>';
+		widgetHtml += '<div style="text-align: center; font-size:12px; color:#333;"><p>Plagiarism checker provided by <a href="https://www.ehmasroy.com/" id="ppsLink" target="_blank"  style="color:#000;">ehmasroy.com</a></p></div>';
 	}
 }
 
