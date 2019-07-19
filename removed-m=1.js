26d1029fa1afdde1fccfedce5c084d59ceeebedd
Royhan Hanafi <50220887+developersroy@users.noreply.github.com>
2019-07-19 11:20:26 +0700

    Update removed-m=1.js

--- removed-m=1.js
+++ removed-m=1.js
@@ -1,4 +1,3 @@
-<script type='text/javascript'>
 var uri = window.location.toString();
 if (uri.indexOf("%3D","%3D") > 0) {
 var clean_uri = uri.substring(0, uri.indexOf("%3D"));
@@ -19,4 +18,3 @@ if (uri.indexOf("?m=1","?m=1") > 0) {
 var clean_uri = uri.substring(0, uri.indexOf("?m=1"));
 window.history.replaceState({}, document.title, clean_uri);
 }
-</script>
